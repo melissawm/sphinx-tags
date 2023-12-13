c35c6fdf2626ba72ce44f22c0ac3a69e22e75e4e
hannah <story645@gmail.com>
2023-12-13 02:27:10 -0500

    doc: allowing tags in body

--- test/sources/test-rst/index.rst
+++ test/sources/test-rst/index.rst
@@ -3,7 +3,9 @@ Test Doc
 Test document
 
 .. toctree::
-    page_1
-    page_2
-    subdir/index
-    _tags/tagsindex
+
+   page_1
+   page_2
+   page_5
+   subdir/index
+   _tags/tagsindex
--- test/test_general_tags.py
+++ test/test_general_tags.py
@@ -1,10 +1,16 @@
 """General tests for tag index and tag pages"""
 from io import StringIO
 from pathlib import Path
-from test.conftest import OUTPUT_ROOT_DIR
+from unittest.mock import MagicMock
 
 import pytest
+
+from sphinx.errors import ExtensionError
 from sphinx.testing.util import SphinxTestApp
+
+from sphinx_tags import TagLinks
+
+from test.conftest import OUTPUT_ROOT_DIR
 
 OUTPUT_DIR = OUTPUT_ROOT_DIR / "general"
 
@@ -25,7 +31,7 @@ def run_all_formats():
 
 @run_all_formats()
 def test_build(app: SphinxTestApp, status: StringIO, warning: StringIO):
-    app.build()
+    app.build(force_all=True)
     assert "build succeeded" in status.getvalue()
 
     # Build with notebooks and text output results in spurious errors "File Not Found: _tags/*.html"
@@ -34,41 +40,68 @@ def test_build(app: SphinxTestApp, status: StringIO, warning: StringIO):
         assert not warning.getvalue().strip()
 
 
+@pytest.mark.sphinx(confoverrides={"tags_create_tags": True})
 @run_all_formats()
 def test_index(app: SphinxTestApp):
-    app.build()
+    app.build(force_all=True)
     build_dir = Path(app.srcdir) / "_build" / "text"
-
     # Check tags index page
-    contents = (build_dir / "_tags" / "tagsindex.txt").read_text()
-    expected_contents = (OUTPUT_DIR / "_tags" / "tagsindex.txt").read_text()
-    assert contents == expected_contents
+    contents = build_dir / "_tags" / "tagsindex.txt"
+    expected_contents = OUTPUT_DIR / "_tags" / "tagsindex.txt"
+    with open(contents, "r") as actual, open(expected_contents, "r") as expected:
+        assert actual.readlines() == expected.readlines()
 
     # Check full toctree created by index
-    contents = (build_dir / "index.txt").read_text()
-    expected_contents = (OUTPUT_DIR / "index.txt").read_text()
-    assert contents == expected_contents
+    contents = build_dir / "index.txt"
+    expected_contents = OUTPUT_DIR / "index.txt"
+    with open(contents, "r") as actual, open(expected_contents, "r") as expected:
+        assert actual.readlines() == expected.readlines()
 
 
+@pytest.mark.sphinx(confoverrides={"tags_create_tags": True})
 @run_all_formats()
 def test_tag_pages(app: SphinxTestApp):
-    app.build()
+    app.build(force_all=True)
     build_dir = Path(app.srcdir) / "_build" / "text"
 
     # Check all expected tag pages
     for tag in ["tag_1", "tag2", "tag-3", "tag-4", "tag_5", "test-tag-please-ignore"]:
-        contents = (build_dir / "_tags" / f"{tag}.txt").read_text()
-        expected_contents = (OUTPUT_DIR / "_tags" / f"{tag}.txt").read_text()
-        assert contents == expected_contents
+        contents = build_dir / "_tags" / f"{tag}.txt"
+        expected_contents = OUTPUT_DIR / "_tags" / f"{tag}.txt"
+        with open(contents, "r") as actual, open(expected_contents, "r") as expected:
+            assert actual.readlines() == expected.readlines()
 
 
 @run_all_formats()
 def test_tagged_pages(app: SphinxTestApp):
-    app.build()
+    app.build(force_all=True)
     build_dir = Path(app.srcdir) / "_build" / "text"
 
     # Check all expected tag pages
-    for page in [Path("page_1.txt"), Path("page_2.txt"), Path("subdir") / "page_3.txt"]:
-        contents = (build_dir / page).read_text()
-        expected_contents = (OUTPUT_DIR / page).read_text()
-        assert contents == expected_contents
+    for page in [
+        Path("page_1.txt"),
+        Path("page_2.txt"),
+        Path("page_5.txt"),
+        Path("subdir") / "page_3.txt",
+    ]:
+        contents = build_dir / page
+        expected_contents = OUTPUT_DIR / page
+        with open(contents, "r") as actual, open(expected_contents, "r") as expected:
+            assert actual.readlines() == expected.readlines()
+
+
+def test_empty_taglinks():
+    tag_links = TagLinks(
+        "tags",
+        "",
+        {},
+        [],
+        0,
+        0,
+        "",
+        MagicMock(),
+        MagicMock(),
+    )
+    msg = "No tags passed to 'tags' directive"
+    with pytest.raises(ExtensionError, match=msg):
+        tag_links.run()
